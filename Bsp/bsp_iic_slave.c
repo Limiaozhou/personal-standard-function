7378c27c336734c89f615f81c901e123760ff221
lmz-rhz <limiaozhou@wissea.com>
2019-08-05 19:56:28 +0800

    整理IIC从机1

--- Bsp/bsp_iic_slave.c
+++ Bsp/bsp_iic_slave.c
@@ -1,19 +1,34 @@
 #include "bsp_iic_master.h"
 #include "bsp_iic_slave.h"
 
+iic_slave_be_written  port1_be_written;  //IIC端口1被写函数
+iic_slave_be_read     port1_be_read;  //IIC端口1被读函数
+
+uint8_t start_flag = 1;  //接收起始信号标志，成功清0
+uint8_t stop_flag = 1;  //接收停止信号标志，成功清0
+
+static uint8_t IIC_Slave_Start(uint8_t port);  //从机检测起始信号
+static uint8_t IIC_Slave_Stop(uint8_t port);  //从机检测停止信号
+static void IIC_Slave_SendAck(uint8_t port, uint8_t ack);  //从机发送应答信号
+static uint8_t IIC_Slave_ReceiveAck(uint8_t port);  //从机接收应答信号
+
+
 //初始化，SCL关中断，SDA开下降沿中断，以检测开始信号
-void IIC_Slave_Init(uint8_t type)
+void IIC_Slave_Init(uint8_t port, iic_slave_be_written be_written_cb, iic_slave_be_read be_read_cb)
 {
-    switch(type)
+    switch(port)
     {
         case 1 :
-            IIC_SLAVE_SDA_DIR_IN();  //SDA输入
+            port1_be_written = be_written_cb;  //将被写回调函数，赋给IIC端口1被写函数
+			port1_be_read = be_read_cb;  //将被读回调函数，赋给IIC端口1被读函数
+			
+			IIC_SLAVE_SDA_DIR_IN();  //SDA输入
             IIC_SLAVE_SCL_DIR_IN();
             IIC_SLAVE_SDA_UP();  //SDA上拉输入，推挽输出
             IIC_SLAVE_SCL_UP();  //SCL上拉输入，推挽输出
             IIC_SLAVE_SDA_EXTI_DOWN();  //SDA下降沿触发，以接收起始信号
-            IIC_SLAVE_SCL_EXTI_UP();  //SCL上升沿触发
-            IIC_SLAVE_SDA_IQ_ON();  //SDA中断输入开，高速输出，以接收起始信号
+            // IIC_SLAVE_SCL_EXTI_UP();  //SCL上升沿触发，与SDA为同一端口，与SDA之间选一配置
+            IIC_SLAVE_SDA_IQ_ON();  //SDA中断输入开，高速输出
             IIC_SLAVE_SCL_IQ_OFF();  //SCL中断输入关，低速输出
         break;
         
@@ -23,17 +38,18 @@ void IIC_Slave_Init(uint8_t type)
 }
 
 //从机检测起始信号，SDA下降沿，SCL处于高电平，成功放回0
-static uint8_t IIC_Slave_Start(uint8_t type)
+static uint8_t IIC_Slave_Start(uint8_t port)
 {
-    switch(type)
+    switch(port)
     {
         case 1 :
-            if(IIC_SLAVE_SCL_PIN_IN)  //SCL处于高电平，成功
+            if(IIC_SLAVE_SCL_PIN_IN && (IIC_SLAVE_SDA_EXTI_EDGE == 0x02))  //SCL处于高电平，SDA下降沿
             {
                 IIC_SLAVE_SDA_IQ_OFF();  //SDA关中断
+				// IIC_SLAVE_SDA_EXTI_UP();  //SDA上升沿触发，以接收停止信号
                 IIC_SLAVE_SCL_EXTI_UP();  //设置SCL上升沿触发，以在SCL高电平时检测SDA电平，接收数据信号
                 IIC_SLAVE_SCL_IQ_ON();  //SCL开中断
-                return 0;  //返回0
+                return 0;  //成功返回0
             }
         break;
         
@@ -45,17 +61,17 @@ static uint8_t IIC_Slave_Start(uint8_t type)
 }
 
 //从机检测停止信号，SDA上升沿，SCL处于高电平，成功放回0
-static uint8_t IIC_Slave_Stop(uint8_t type)
+static uint8_t IIC_Slave_Stop(uint8_t port)
 {
-   switch(type)
+   switch(port)
    {
        case 1 :
-           if(IIC_SLAVE_SCL_PIN_IN)  //SCL处于高电平，成功
+           if(IIC_SLAVE_SCL_PIN_IN && (IIC_SLAVE_SDA_EXTI_EDGE == 0x01))  //SCL处于高电平，SDA上升沿
            {
                IIC_SLAVE_SDA_DIR_IN();  //SDA输入
                IIC_SLAVE_SDA_EXTI_DOWN();  //SDA下降沿触发，以接收起始信号
-               IIC_SLAVE_SCL_EXTI_UP();  //SCL上升沿触发
-               IIC_SLAVE_SDA_IQ_ON();  //SDA中断输入开，高速输出，以接收起始信号
+               // IIC_SLAVE_SCL_EXTI_UP();  //SCL上升沿触发
+               IIC_SLAVE_SDA_IQ_ON();  //SDA中断输入开，高速输出
                IIC_SLAVE_SCL_IQ_OFF();  //SCL中断输入关，低速输出
                return 0;  //成功返回0
            }
@@ -69,9 +85,9 @@ static uint8_t IIC_Slave_Stop(uint8_t type)
 }
 
 //从机发送应答信号，0:ACK，1:NAK
-static void IIC_Slave_SendAck(uint8_t type, uint8_t ack)
+static void IIC_Slave_SendAck(uint8_t port, uint8_t ack)
 {
-   switch(type)
+   switch(port)
    {
        case 1 :
 		   IIC_SLAVE_SDA_DIR_OUT();
@@ -85,11 +101,11 @@ static void IIC_Slave_SendAck(uint8_t type, uint8_t ack)
 }
 
 //从机接收应答信号，0:ACK，1:NAK
-static uint8_t IIC_Slave_ReceiveAck(uint8_t type)
+static uint8_t IIC_Slave_ReceiveAck(uint8_t port)
 {
    uint8_t ack = 1;
    
-   switch(type)
+   switch(port)
    {
        case 1 :
            ack = IIC_SLAVE_SDA_PIN_IN;
@@ -102,91 +118,121 @@ static uint8_t IIC_Slave_ReceiveAck(uint8_t type)
    return ack;
 }
 
-//从机发送数据信号
-static void IIC_Slave_SendBit(uint8_t type, uint8_t bit)
+//从机发送数据信号，发完一字节返回应答信号，否则返回-1
+static int8_t IIC_Slave_SendByte(uint8_t port, uint8_t byte)
 {
-    switch(type)
-    {
-        case 1 :
-			IIC_SLAVE_SDA_DIR_OUT();
-			IIC_SLAVE_SDA_OUT(bit);
-			IIC_SLAVE_SDA_DIR_IN();
-        break;
-        
-        default :
-        break;
-    }
-
-    return byte;
-}
-
-//从机接收一字节
-static uint8_t IIC_Slave_ReceiveByte(uint8_t type)
-{
-    uint8_t byte = 0;  //数据字节缓存
-    
-    switch(type)
+	uint8_t bit = 1;
+	
+    switch(port)
     {
         case 1 :
 			static uint8_t i = 0;  //数据bit位数记录
 			
-            if(i++ >= 7)  //接收7个bit后返回一个字节
-            {
-                byte |= IIC_SLAVE_SDA_PIN_IN << (7 - i);
+			IIC_SLAVE_SDA_DIR_OUT();
+			
+			if(i++ < 8)  //前8个脉冲发送数据各bit位
+			{
+				bit = !!(byte & (0x80 >> i));  //取逻辑值0或1，否则P0_0=bit时，只读取bit数据的最低位bit0的值
+				IIC_SLAVE_SDA_OUT(bit);
+			}
+			else  //第9个脉冲接收应答，并返回应答信号
+			{
+				IIC_SLAVE_SDA_DIR_IN();
 				i = 0;
-            }
+				return IIC_Slave_ReceiveAck(port);
+			}
         break;
         
         default :
         break;
     }
-
-    return byte;
+	
+	return -1;
 }
 
-//从机接收指定长度数据
-static uint8_t IIC_Slave_Read(uint8_t type, uint8_t * data, uint8_t len)
+//从机接收一字节，接收完一个字节，再接收第9个脉冲时返回0，否则返回1
+static uint8_t IIC_Slave_ReceiveByte(uint8_t port, uint8_t * byte)
 {
-    uint8_t i;
-    
-    switch(type)
+    switch(port)
     {
         case 1 :
-            for(i = 0; i < len; i++)
+			static uint8_t i = 0;  //数据bit位数记录
+			
+			if(!i)
+				*byte = 0;  //开始清0，之后再赋值
+			
+            if(i++ < 8)
+                *byte |= IIC_SLAVE_SDA_PIN_IN << (7 - i);
+			else
 			{
-				*(data + i) = IIC_Slave_ReceiveByte(type);
-				if(i < len - 1)
-					IIC_Slave_SendAck(type, 0);  //接收一个字节，发送一个有效应答信号
+				i = 0;
+				return 0;
 			}
-			IIC_Slave_SendAck(type, 1);  //接收完数据，发送一个非应答信号
         break;
         
         default :
         break;
     }
 
-    return byte;
+    return 1;
 }
 
-//
-void IIC_Slave_Deal(uint8_t type)
+//从机发送指定长度数据
+
+
+//从机接收指定长度数据
+static uint8_t IIC_Slave_Read(uint8_t port, uint8_t * data, uint8_t len)
 {
-    static uint8_t state = 0;
-    
-    
-    if((!state) && (!IIC_Slave_Start(type)))
-        state = 1;  //收到起始信号，并自锁
-    
-    if(state)
+    switch(port)
     {
-        if(IIC_Slave_ReceiveByte(type) == SlaveAddress)
-            ;
+        case 1 :
+			static uint8_t i = 0;  //数据字节数记录
+			static uint8_t byte = 0;  //数据缓存
+			
+			if(!IIC_Slave_ReceiveByte(port, &byte))
+			{
+				*(data + i) = byte;  //接收完一个字节再赋值
+				if(i < len - 1)
+				{
+					IIC_Slave_SendAck(port, 0);  //接收完一个字节，发送一个有效应答信号
+					i++;
+				}
+				else
+				{
+					IIC_Slave_SendAck(port, 1);  //接收完所有数据，发送一个非应答信号
+					i = 0;
+				}
+			}
+        break;
+        
+        default :
+        break;
     }
+}
+
+//TTC从机接收启停信号
+void IIC_Slave_Start_Judge(uint8_t port)
+{
+	if(start_flag)  //接收起始信号后自锁，不再接收，直到满足重新接收条件
+		start_flag = IIC_Slave_Start(port);  //接收起始成功，为0
+	
+	if(!IIC_Slave_Stop(port))  //接收停止信号成功，为0
+		start_flag = 1;
+}
+
+//IIC从机处理数据信号
+void IIC_Slave_Data_Deal(uint8_t port)
+{
+	
+	
+	if(!start_flag)  
+	{
+		
+	}
 }
 
 //STM8 PB口接收中断函数，SDA、SCL引脚
 #pragma vector=EXTI1_vector  //6
 __interrupt void EXTI1_IRQHandler(void)
 {
-    
 }
--- Bsp/bsp_iic_slave.h
+++ Bsp/bsp_iic_slave.h
@@ -19,50 +19,45 @@ typedef long int int32_t;
 #define HIGH 1  //引脚输出电平-高
 #define LOW  0
 
-#define OUT 1  //引脚配置输出
-#define IN  0
-
-#define DELAY_US_IIC  5  //IIC输出延时us
-
 #define my_st(x)  do { x } while(0)  //函数语句
-#define my_BV(n)  (1 << (n))  //取n位
+#define my_BV(n)  (1 << (n))  //取n位为1
 
 #define IICAddress 0x3C  //本机IIC地址
 
 //IIC引脚配置寄存器定义
-//type = 1
-#define IIC_SLAVE_SDA_CR1       PB_CR1_C15  //数据线SDA控制寄存器1
-#define IIC_SLAVE_SCL_CR1       PB_CR1_C14  //时钟线SCL
+//port = 1
+#define IIC_SLAVE_SDA_PULL       PB_CR1_C15  //数据线SDA控制寄存器1，控制上下拉
+#define IIC_SLAVE_SCL_PULL       PB_CR1_C14  //时钟线SCL
 
-#define IIC_SLAVE_SDA_CR2       PB_CR2_C25  //数据线SDA控制寄存器2
-#define IIC_SLAVE_SCL_CR2       PB_CR2_C24  //时钟线SCL
+#define IIC_SLAVE_SDA_IQ         PB_CR2_C25  //数据线SDA控制寄存器2，控制中断开关
+#define IIC_SLAVE_SCL_IQ         PB_CR2_C24  //时钟线SCL
 
-#define IIC_SLAVE_SDA_PIN_OUT   PB_ODR_ODR5  //数据线SDA输出引脚
-#define IIC_SLAVE_SCL_PIN_OUT   PB_ODR_ODR4  //时钟线SCL
+#define IIC_SLAVE_SDA_PIN_OUT    PB_ODR_ODR5  //数据线SDA输出引脚
+#define IIC_SLAVE_SCL_PIN_OUT    PB_ODR_ODR4  //时钟线SCL
 
-#define IIC_SLAVE_SDA_PIN_IN    PB_IDR_IDR5  //数据线SDA输入引脚
-#define IIC_SLAVE_SCL_PIN_IN    PB_IDR_IDR4  //时钟线SCL
+#define IIC_SLAVE_SDA_PIN_IN     PB_IDR_IDR5  //数据线SDA输入引脚
+#define IIC_SLAVE_SCL_PIN_IN     PB_IDR_IDR4  //时钟线SCL
 
-#define IIC_SLAVE_SDA_DIR       PB_DDR  //SDA输入/输出选择寄存器
-#define IIC_SLAVE_SCL_DIR       PB_DDR
+#define IIC_SLAVE_SDA_DIR        PB_DDR  //SDA输入/输出选择寄存器
+#define IIC_SLAVE_SCL_DIR        PB_DDR
 
-#define IIC_SLAVE_SDA_DIR_BV    my_BV(5)  //SDA输入/输出寄存器位选择
-#define IIC_SLAVE_SCL_DIR_BV    my_BV(4)
+#define IIC_SLAVE_SDA_DIR_BV     my_BV(5)  //SDA输入/输出寄存器位选择
+#define IIC_SLAVE_SCL_DIR_BV     my_BV(4)
 
-#define IIC_SLAVE_SDA_EXTI_CR1  EXTI_CR1_PBIS  //数据线SDA中断寄存器1
-#define IIC_SLAVE_SCL_EXTI_CR1  EXTI_CR1_PBIS  //时钟线SCL中断寄存器1
+#define IIC_SLAVE_SDA_EXTI_EDGE  EXTI_CR1_PBIS  //数据线SDA中断寄存器1
+#define IIC_SLAVE_SCL_EXTI_EDGE  EXTI_CR1_PBIS  //时钟线SCL中断寄存器1
 
-#define IIC_SLAVE_SDA_UP()           my_st(IIC_SLAVE_SDA_CR1 = 1;)  //SDA上拉输入，推挽输出
-#define IIC_SLAVE_SDA_FLOAT()        my_st(IIC_SLAVE_SDA_CR1 = 0;)  //SDA浮空输入，开漏输出
+#define IIC_SLAVE_SDA_UP()           my_st(IIC_SLAVE_SDA_PULL = 1;)  //SDA上拉输入，推挽输出
+#define IIC_SLAVE_SDA_FLOAT()        my_st(IIC_SLAVE_SDA_PULL = 0;)  //SDA浮空输入，开漏输出
 
-#define IIC_SLAVE_SCL_UP()           my_st(IIC_SLAVE_SCL_CR1 = 1;)  //SCL上拉输入，推挽输出
-#define IIC_SLAVE_SCL_FLOAT()        my_st(IIC_SLAVE_SCL_CR1 = 0;)  //SCL浮空输入，开漏输出
+#define IIC_SLAVE_SCL_UP()           my_st(IIC_SLAVE_SCL_PULL = 1;)  //SCL上拉输入，推挽输出
+#define IIC_SLAVE_SCL_FLOAT()        my_st(IIC_SLAVE_SCL_PULL = 0;)  //SCL浮空输入，开漏输出
 
-#define IIC_SLAVE_SDA_IQ_ON()        my_st(IIC_SLAVE_SDA_CR2 = 1;)  //SDA中断输入开，高速输出
-#define IIC_SLAVE_SDA_IQ_OFF()       my_st(IIC_SLAVE_SDA_CR2 = 0;)  //SDA中断输入关，低速输出
+#define IIC_SLAVE_SDA_IQ_ON()        my_st(IIC_SLAVE_SDA_IQ = 1;)  //SDA中断输入开，高速输出
+#define IIC_SLAVE_SDA_IQ_OFF()       my_st(IIC_SLAVE_SDA_IQ = 0;)  //SDA中断输入关，低速输出
 
-#define IIC_SLAVE_SCL_IQ_ON()        my_st(IIC_SLAVE_SCL_CR2 = 1;)  //SCL中断输入开，高速输出
-#define IIC_SLAVE_SCL_IQ_OFF()       my_st(IIC_SLAVE_SCL_CR2 = 0;)  //SCL中断输入关，低速输出
+#define IIC_SLAVE_SCL_IQ_ON()        my_st(IIC_SLAVE_SCL_IQ = 1;)  //SCL中断输入开，高速输出
+#define IIC_SLAVE_SCL_IQ_OFF()       my_st(IIC_SLAVE_SCL_IQ = 0;)  //SCL中断输入关，低速输出
 
 #define IIC_SLAVE_SDA_OUT(a)         my_st(IIC_SLAVE_SDA_PIN_OUT = a;)  //SDA输出电平
 #define IIC_SLAVE_SCL_OUT(a)         my_st(IIC_SLAVE_SCL_PIN_OUT = a;)
@@ -73,15 +68,18 @@ typedef long int int32_t;
 #define IIC_SLAVE_SCL_DIR_OUT()      my_st(IIC_SLAVE_SCL_DIR |= IIC_SLAVE_SCL_DIR_BV;)
 #define IIC_SLAVE_SCL_DIR_IN()       my_st(IIC_SLAVE_SCL_DIR &= ~(IIC_SLAVE_SCL_DIR_BV);)
 
-#define IIC_SLAVE_SDA_EXTI_UP()      my_st(IIC_SLAVE_SDA_EXTI_CR1 = 0x01;)  //SDA中断上升沿触发
-#define IIC_SLAVE_SDA_EXTI_DOWN()    my_st(IIC_SLAVE_SDA_EXTI_CR1 = 0x02;)  //SDA中断下降沿触发
-#define IIC_SLAVE_SDA_EXTI_UPDOWN()  my_st(IIC_SLAVE_SDA_EXTI_CR1 = 0x03;)  //SDA中断上升和下降沿触发
+#define IIC_SLAVE_SDA_EXTI_UP()      my_st(IIC_SLAVE_SDA_EXTI_EDGE = 0x01;)  //SDA中断上升沿触发
+#define IIC_SLAVE_SDA_EXTI_DOWN()    my_st(IIC_SLAVE_SDA_EXTI_EDGE = 0x02;)  //SDA中断下降沿触发
+#define IIC_SLAVE_SDA_EXTI_UPDOWN()  my_st(IIC_SLAVE_SDA_EXTI_EDGE = 0x03;)  //SDA中断上升和下降沿触发
     
-#define IIC_SLAVE_SCL_EXTI_UP()      my_st(IIC_SLAVE_SCL_EXTI_CR1 = 0x01;)  //SCL中断上升沿触发
-#define IIC_SLAVE_SCL_EXTI_DOWN()    my_st(IIC_SLAVE_SCL_EXTI_CR1 = 0x02;)  //SCL中断下降沿触发
-#define IIC_SLAVE_SCL_EXTI_UPDOWN()  my_st(IIC_SLAVE_SCL_EXTI_CR1 = 0x03;)  //SCL中断上升和下降沿触发
+#define IIC_SLAVE_SCL_EXTI_UP()      my_st(IIC_SLAVE_SCL_EXTI_EDGE = 0x01;)  //SCL中断上升沿触发
+#define IIC_SLAVE_SCL_EXTI_DOWN()    my_st(IIC_SLAVE_SCL_EXTI_EDGE = 0x02;)  //SCL中断下降沿触发
+#define IIC_SLAVE_SCL_EXTI_UPDOWN()  my_st(IIC_SLAVE_SCL_EXTI_EDGE = 0x03;)  //SCL中断上升和下降沿触发
 
-void IIC_Slave_Init(uint8_t type);
+typedef void (*iic_slave_be_written)(uint16_t reg_adr, uint8_t *data, uint16_t len);  //IIC从机被写回调函数，接收指定长度数据到指定寄存器
+typedef void (*iic_slave_be_read)(uint16_t reg_adr, uint8_t *data, uint16_t len);  //IIC从机被读回调函数，发送指定长度数据
+
+void IIC_Slave_Init(uint8_t port, iic_slave_be_written be_written_cb, iic_slave_be_read be_read_cb);  //IIC从机初始化，注册读写回调函数
 
 #ifdef __cplusplus
 }
