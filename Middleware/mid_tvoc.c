515ea620c0574e3178205090971947b08df2d854
lmz-rhz <limiaozhou@wissea.com>
2019-09-17 15:17:47 +0800

    修改TVOC 1

--- Middleware/mid_tvoc.c
+++ Middleware/mid_tvoc.c
@@ -1,70 +1,29 @@
 #include "mid_temperature.h"
 
-// IIC_Master_WRInfo_TypeDef iic_info =
-// {
-	// .port = 3,  //端口2
-	// .device_adr = TEMP_DEVICE_ADDRESS,  //地址0xEE
-	// .dev_adr_tenbit_flag = 0,  //非10位地址
-// //	.data = NULL,  //初始数据指针空
-	// .len = 1,  //初始数据长度1
-	// .error_resend_times = 3,  //失败重读次数3
-// };  //IIC读取信息
-
-static IIC_Master_ReadReg_Info_TypeDef iic_info =
+IIC_Hard_Master_WRInfo_TypeDef iic_info =
 {
-	.wr_info.port = 3,
-	.wr_info.device_adr = TEMP_DEVICE_ADDRESS,
-	.wr_info.dev_adr_tenbit_flag = 0,
-	// .wr_info.data = buf,
-	.wr_info.len = 1,
-	.wr_info.error_resend_times = 3,
-	.register_adr = 0x00,
-	.reg_adr_twobyte_flag = 0,
-	.readreg_delay_flag = Delay_Front,
-	.readreg_delay_nms = 90
-};
+	.device_adr = TVOC_DEVICE_ADDRESS,  //地址0xEE
+	.dev_adr_tenbit_flag = 0,  //非10位地址
+	.len = 13,  //初始数据长度1
+	.error_resend_times = 3,  //失败重读次数3
+};  //IIC读取信息
 
 static uint8_t temp_dev_reset(void);  //设备复位，成功返回0，失败返回1
 static uint8_t digital_data_read(uint16_t * data, Temp_Reg_TypeDef reg);  //读取气压或温度数据，成功返回0，失败返回1
 static uint8_t temp_humi_convert(float * temp_data, float * humi_data);  //温湿度数据读取并计算函数，成功返回0，失败返回1
 
-//气压及其温度读取函数，成功返回0，失败返回1
-uint8_t temp_humi_read(float * temp_data, float * humi_data)
+//tvoc及其co2读取函数，成功返回0，失败返回1
+uint8_t tvoc_eco2_read(float * tvoc_data, float * eco2_data)
 {
-	static uint8_t reset_flag = 1;  //复位标志
-	
-	if(reset_flag)
-	{
-		if(temp_dev_reset())  //复位，并判断是否成功
-			return 1;
-		reset_flag = 0;
-		delay_ms(20);
-	}
-	
 	if(temp_humi_convert(temp_data, humi_data))  //气压及其温度数据读取并计算，并判断是否成功
 	{
-		reset_flag = 1;
 		return 1;
 	}
 	
 	return 0;
 }
 
-//设备复位，成功返回0，失败返回1
-static uint8_t temp_dev_reset(void)
-{
-	uint8_t result = 1;
-	uint8_t command = TEMP_DEVICE_RESET;
-	
-	iic_info.wr_info.data = &command;
-	iic_info.wr_info.len = 1;
-	
-	result = IIC_Master_Write(&(iic_info.wr_info));
-	
-    return result;
-}
-
-//读取气压或温度数据，成功返回0，失败返回1
+//读取tvoc数据，成功返回0，失败返回1
 static uint8_t digital_data_read(uint16_t * data, Temp_Reg_TypeDef reg)
 {
     uint8_t buf[3] = {0};  //缓存数组，前两字节数据，后一字节校验
--- Middleware/mid_tvoc.h
+++ Middleware/mid_tvoc.h
@@ -3,20 +3,9 @@
 
 #include "bsp_iic.h"
 
-typedef enum
-{
-	Temp_Host = 0xE3,
-	Humi_Host = 0xE5,
-	Temp_NoHost = 0xF3,
-	Humi_NoHost = 0xF5
-}Temp_Reg_TypeDef;
+#define TVOC_DEVICE_ADDRESS 0x80  //设备地址
 
-#define TEMP_DEVICE_ADDRESS 0x80  //设备地址
-#define TEMP_DEVICE_RESET 0xFE  //设备地址
-
-#define DELAY_TEMP 90  //等待ADC转化延时时间
-
-//温湿度读取函数，成功返回0，失败返回1
-uint8_t temp_humi_read(float * temp_data, float * humi_data);
+//tvoc读取函数，成功返回0，失败返回1
+uint8_t tvoc_eco2_read(float * tvoc_data, float * eco2_data);
 
 #endif
